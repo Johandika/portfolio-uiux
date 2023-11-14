0283657831e5ad925fa969f2e32bf0a243fde8c8
Johandika <johanelyosse@gmail.com>
2023-11-14 17:09:37 +0700

    Update skill

--- src/assets/components/Skills.jsx
+++ src/assets/components/Skills.jsx
@@ -1,22 +1,12 @@
 import React from "react";
-import codingskill1 from "../../assets/images/codingskill1.png";
-import codingskill2 from "../../assets/images/codingskill2.png";
-import codingskill3 from "../../assets/images/codingskill3.png";
-import codingskill4 from "../../assets/images/codingskill4.png";
-import codingskill5 from "../../assets/images/codingskill5.png";
-import codingskill6 from "../../assets/images/codingskill6.png";
-import designskill1 from "../../assets/images/designskill1.png";
-import designskill2 from "../../assets/images/designskill2.png";
-import designskill3 from "../../assets/images/designskill3.png";
-import designskill4 from "../../assets/images/designskill4.png";
-import designskill5 from "../../assets/images/designskill5.png";
-import designskill6 from "../../assets/images/designskill6.png";
+import {nextjs,html,css,js,tailwind,nodejs,expressjs,redux,postgre,dbeaver,logopostman, reactjs,bootstrap, designskill1,designskill2,designskill3,designskill4,designskill5,designskill6} from "../images/index.js";
+
 
 export default function Skills() {
   return (
     <section id="skills">
       <div className="skill1 container-fluid text-center pt-4">
-        <div className=" row">
+        <div className="mb-4  row">
           <div className="col-12">
             <h6>Coding Skills</h6>
           </div>
@@ -24,34 +14,70 @@ export default function Skills() {
         <div className="row-skill row 4 mt-2 mx-auto">
           <div className="col-md-2 col-4 mb-4">
             <a href="https://www.w3schools.com/html/" target="_blank">
-              <img src={codingskill1} alt="HTML Logo" />
+              <img src={html} alt="HTML Logo" />
             </a>
           </div>
           <div className="col-md-2 col-4 mb-4">
             <a href="https://www.w3schools.com/css/default.asp" target="_blank">
-              <img src={codingskill2} alt="CSS Logo" />
+              <img src={css} alt="CSS Logo"  />
             </a>
           </div>
           <div className="col-md-2 col-4 mb-4">
             <a href="https://www.w3schools.com/js/default.asp" target="_blank">
-              <img src={codingskill3} alt="JS Logo" />
+              <img src={js} alt="JS Logo"  />
             </a>
           </div>
-          <div className="col-md-2 col-4 mb-4">
+          <div className="col-md-2 col-4 mb-4 ">
             <a href="https://laravel.com/" target="_blank">
-              <img src={codingskill4} alt="Laravel Logo" />
+              <img src={nextjs} alt="Next JS Logo"  />
             </a>
           </div>
           <div className="col-md-2 col-4 mb-4">
             <a href="https://react.dev/" target="_blank">
-              <img src={codingskill5} alt="React Logo" />
+              <img src={reactjs} alt="React Logo"  />
+            </a>
+          </div>
+          <div className="col-md-2 col-4 mb-4">
+            <a href="https://react.dev/" target="_blank">
+              <img src={tailwind} alt="Tailwind"  />
             </a>
           </div>
           <div className="col-md-2 col-4 mb-4">
             <a href="https://getbootstrap.com/" target="_blank">
-              <img src={codingskill6} alt="Bootstrap Logo" />
+              <img src={bootstrap} alt="Bootstrap Logo"  />
             </a>
           </div>
+          <div className="col-md-2 col-4 mb-4">
+            <a href="https://getbootstrap.com/" target="_blank">
+              <img src={nodejs} alt="Nodejs Logo"  />
+            </a>
+          </div>
+          <div className="col-md-2 col-4 mb-4">
+            <a href="https://getbootstrap.com/" target="_blank">
+              <img src={expressjs} alt="Expressjs" />
+            </a>
+          </div>
+          <div className="col-md-2 col-4 mb-4">
+            <a href="https://getbootstrap.com/" target="_blank">
+              <img src={redux} alt="Redux logo"  />
+            </a>
+          </div>
+          <div className="col-md-2 col-4 mb-4">
+            <a href="https://getbootstrap.com/" target="_blank">
+              <img src={dbeaver} alt="Dbeaver logo"  />
+            </a>
+          </div>
+          <div className="col-md-2 col-4 mb-4">
+            <a href="https://getbootstrap.com/" target="_blank">
+              <img src={postgre} alt="Postgre logo"  />
+            </a>
+          </div>
+          <div className="col-md-2 col-4 mb-4">
+            <a href="https://getbootstrap.com/" target="_blank">
+              <img src={logopostman} alt="Postgre logo"  />
+            </a>
+          </div>
+         
         </div>
       </div>
 
---
+++ src/assets/images/index.js
@@ -0,0 +1,22 @@
+import nextjs from './nextjs.png';
+import html from './html.png';
+import css from './css.png';
+import js from './js.png';
+import tailwind from './tailwind.png';
+import nodejs from './nodejs.png';
+import expressjs from './expressjs.png';
+import redux from './redux.png';
+import postgre from './postgre.png';
+import dbeaver from './dbeaver.png';
+import logopostman from './logopostman.png';
+import reactjs from './reactjs.png';
+import bootstrap from './bootstrap.png';
+
+import designskill1 from './designskill1.png';
+import designskill2 from './designskill2.png';
+import designskill3 from './designskill3.png';
+import designskill4 from './designskill4.png';
+import designskill5 from './designskill5.png';
+import designskill6 from './designskill6.png';
+
+export {nextjs,html,css,js,tailwind,nodejs,expressjs,redux,postgre,dbeaver,logopostman,reactjs,bootstrap, designskill1,designskill2,designskill3,designskill4,designskill5,designskill6}
